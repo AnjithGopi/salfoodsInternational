14b7f6a89de68a2c93ed8062eb3d844a00b16f91
AnjithGopi <ng.anjith444@gmail.com>
2026-01-23 01:39:44 +0530

    some minor updations in map

--- src/coordinates/Coordinates.jsx
+++ src/coordinates/Coordinates.jsx
@@ -1,5 +1,7 @@
+export const coordinates = {
+  latitude: 9.9440426,
+  longitude: 76.2543299,
+  zoo: 15,
+};
 
-export const latitude=9.9440426
-export const longitude=76.2543299
-export const zoom=15
-export const mapSrc = `https://www.google.com/maps?q=${latitude},${longitude}&z=${zoom}&output=embed`;
+export const mapSrc = `https://www.google.com/maps?q=${coordinates.latitude},${coordinates.longitude}&z=${coordinates.zoom}&output=embed`;
