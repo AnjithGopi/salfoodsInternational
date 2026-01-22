8ba815cf48bd36225451f8b1f254950eec3e7877
AnjithGopi <ng.anjith444@gmail.com>
2026-01-22 22:17:32 +0530

    image changed

---
+++ src/contact/Address.jsx
@@ -0,0 +1,9 @@
+export const contact = {
+  address: "10/188 mythri line chullickal cochin-5",
+  pincode: 682005,
+  mobile:"+917994493684",
+  mobile2:"+919946309222",
+  email:"salfoodsinternationals@gmail.com",
+  webSite:"www.salfoodsinternational.com"
+
+};
